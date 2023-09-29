b6c134e53f40c18023dbdf43ce041836a7d756aa
Joshua <joshcscurfield@gmail.com>
2023-09-29 16:39:53 +0100

    added if statement that returns 405 for non-POST api

--- joshauthtestsite/app/api/route.ts
+++ joshauthtestsite/app/api/route.ts
@@ -6,6 +6,9 @@ export default async function handler(
     req: NextRequest
     res: NextResponse 
 ) {
+    if (req.method !== 'POST') {
+        return res.status(405).end():
+    }
     const data = req.body
     const id = await createItem(data)
     res.status(200).json({ id })
