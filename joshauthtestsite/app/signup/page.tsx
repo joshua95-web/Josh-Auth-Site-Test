7c69c0ce97c46590a2a1dab4f448af2318476435
Joshua <joshcscurfield@gmail.com>
2023-10-02 14:55:19 +0100

    added handleChange and handleSubmit functions to process the keystrokes of the input

--- joshauthtestsite/app/signup/page.tsx
+++ joshauthtestsite/app/signup/page.tsx
@@ -6,7 +6,7 @@ event?.preventDefault()
 const [username, setUsername] = useState('')
 const [password, setPassword] = useState('')
 
-function handleUpdate(event: React.ChangeEvent<HTMLInputElement>) {
+function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const value = event.target.value
     const name = event.target.name
     if (name === 'username') {
@@ -16,12 +16,28 @@ function handleUpdate(event: React.ChangeEvent<HTMLInputElement>) {
     }
   }
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>)
+    event?.preventDefault()
+
+}
+
 
     return (
         <div>
-            <form onSubmit={}>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+            <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                name="username"
+                onChange={handleChange} 
+                placeholder="Username" 
+                />
+             <input 
+                type="password"
+                name="password" 
+                onChange={handleChange}
+                placeholder="Password" 
+                />
+                <button type="submit">Submit</button>
         <input type="password" placeholder="Confirm Password" />
         </form>
         </div>
